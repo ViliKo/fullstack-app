943c8e084cfdfbce97d4bb2904e1482af9dc30af
ViliKo <59921526+ViliKo@users.noreply.github.com>
2024-12-18 14:57:42 +0200

    add test.http flie for API endpoint testing

--- app.js
+++ app.js
@@ -1,11 +1,53 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+const path = require("path");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+let counter = 3;
+
+let db = [
+  { id: 1, name: "jack" },
+  { id: 2, name: "tina" },
+];
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/customers", (req, res) => {
+  res.send(db);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.get("/customers/:urlId([0-9]+)", (req, res) => {
+  const id = Number(req.params.urlId);
+  const temp = db.find((customer) => customer.id === id);
+  if (temp) {
+    res.send(temp);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.post("/customers", (req, res) => {
+  const customer = req.body;
+  customer.id = counter++;
+  db.push(customer);
+  res.send(customer);
+});
+
+app.delete("/customers/:urlId([0-9]+)", (req, res) => {
+  const id = Number(req.params.urlId);
+  db = db.filter((customer) => customer.id != id);
+  res.status(204).end();
+});
+
+const server = app.listen(port, () => {
+  console.log(`My app listening on port ${port}`);
+});
+
+process.on("SIGINT", () => {
+  console.log("Gracefully shutting down Express.js server...");
+  server.close(() => {
+    console.log("Server closed.");
+    process.exit(0);
+  });
 });
